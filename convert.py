2e77e2b77ddf6c0c351232730c4d791e8383eed7
missfxhr <xiaofang2016@u.northwestern.edu>
2015-05-26 14:44:43 -0500

    make convertFile more adjustable

--- convert.py
+++ convert.py
@@ -4,19 +4,8 @@ def convertFile(fileName):
     # Index Const:
     DateIndex = 1
     OppTeamIndex = 2
-    AtIndex = 3
     HomeTeamIndex = 4
-    PA2Index = 11
-    PA3Index = 14
-    FTAIndex = 17
-    PTSIndex = 19
-    ORBIndex = 20
-    DRBIndex = 21
-    ASTIndex = 23
-    STLIndex = 24
-    BLKIndex = 25
-    TOVIndex = 26
-    HomeIndexOffset = 21
+    GameInfoIndexLength = 7
     # Game Weights:
     FirstWeight = 0.70
     FirstGameLine = 10
@@ -24,15 +13,27 @@ def convertFile(fileName):
     # Score Difference Levels:
     FirstScoreLine = 5
     SecondScoreLine = 15
-    ScoreDifLvl0 = 1
-    ScoreDifLvl1 = 2
-    ScoreDifLvl2 = 3
     # Attribute Names Dictionary:
-    AttrNamesDictionary = {PA2Index:'2PA',PA3Index:'3PA',FTAIndex:'FTA',PTSIndex:'PTS',ORBIndex:'ORB',DRBIndex:'DRB',ASTIndex:'AST',STLIndex:'STL',BLKIndex:'BLK',TOVIndex:'TOV'}
+    AllAttrNamesDictionary = {}
+    AttrNamesDictionary = {}
     # Get the Data from File:
     csvReader = csv.reader(open(fileName,"rb"),delimiter=',') 
     data = list(csvReader)
     firstRow = data.pop(0)
+    # Initialize Some Const Value and Dictionaries
+    HomeIndexOffset = (len(firstRow)-GameInfoIndexLength)/2
+    for i in range(GameInfoIndexLength,GameInfoIndexLength+HomeIndexOffset):
+        AllAttrNamesDictionary[firstRow[i]] = i
+    AttrNamesDictionary[AllAttrNamesDictionary['2PA']] = '2PA'
+    AttrNamesDictionary[AllAttrNamesDictionary['3PA']] = '3PA'
+    AttrNamesDictionary[AllAttrNamesDictionary['FTA']] = 'FTA'
+    AttrNamesDictionary[AllAttrNamesDictionary['PTS']] = 'PTS'
+    AttrNamesDictionary[AllAttrNamesDictionary['ORB']] = 'ORB'
+    AttrNamesDictionary[AllAttrNamesDictionary['DRB']] = 'DRB'
+    AttrNamesDictionary[AllAttrNamesDictionary['AST']] = 'AST'
+    AttrNamesDictionary[AllAttrNamesDictionary['STL']] = 'STL'
+    AttrNamesDictionary[AllAttrNamesDictionary['BLK']] = 'BLK'
+    AttrNamesDictionary[AllAttrNamesDictionary['TOV']] = 'TOV'
     gamesDictionary = {'TOR': [],'BOS': [],'BRK': [],'PHI': [],'NYK': [],'CLE': [],'CHI': [],'MIL': [],'IND': [],'DET': [],'ATL': [],'WAS': [],'MIA': [],'ORL': [],'POR': [],'OKC': [],'UTA': [],'DEN': [],'MIN': [],'GSW': [],'LAC': [],'PHO': [],'SAC': [],'LAL': [],'HOU': [],'SAS': [],'MEM': [],'DAL': [],'NOP': [],'NOH':[],'CHA': [],'CHO': []}  
     # Generate Games Hash Table with Team Name as Keys:
     for dataRow in data:
@@ -58,7 +59,6 @@ def convertFile(fileName):
             resultLineDictionary = {}
             for attrKey in AttrNamesDictionary:
                 resultLineDictionary[attrKey] = 0
-            resultLineDictionary = {PA2Index:0,PA3Index:0,FTAIndex:0,PTSIndex:0,ORBIndex:0,DRBIndex:0,ASTIndex:0,STLIndex:0,BLKIndex:0,TOVIndex:0}
             for i in range(homeGameIndex):
                 currentWeight = SecondWeight if (homeGameIndex-i)>FirstGameLine else FirstWeight
                 for attrKey in resultLineDictionary:
@@ -68,7 +68,7 @@ def convertFile(fileName):
                 for attrKey in resultLineDictionary:
                     resultLineDictionary[attrKey] -= int(gamesDictionary[oppTeamKey][i][attrKey + (HomeIndexOffset if gamesDictionary[oppTeamKey][i][HomeTeamIndex] == oppTeamKey else 0)]) * currentWeight / oppGameIndex
             # Generate Classified Result:
-            scoreDif = int(dataRow[PTSIndex+HomeIndexOffset])-int(dataRow[PTSIndex])
+            scoreDif = int(dataRow[AllAttrNamesDictionary['PTS']+HomeIndexOffset])-int(dataRow[AllAttrNamesDictionary['PTS']])
             if scoreDif > SecondScoreLine:
                 scoreDifLvl = 'ScoreDifLvl2'
             elif scoreDif < -SecondScoreLine:
