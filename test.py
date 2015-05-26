a6224bb385b022b5e394f80097c5e823f5513e6d
missfxhr <xiaofang2016@u.northwestern.edu>
2015-05-26 15:41:44 -0500

    fix test bug

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import csv
 execfile("convert.py")
 result = []
+firstLine = []
 for i in range(11,15):
     result1, result2 = convertFile(str(i)+'-'+str(i+1)+'.csv')
-    result += result1
+    result += result1[1:]
+    firstLine = result1[0]
+result.insert(0,firstLine)
 with open("result.csv", "wb") as outputFile:
     writer = csv.writer(outputFile)
     writer.writerows(result)
