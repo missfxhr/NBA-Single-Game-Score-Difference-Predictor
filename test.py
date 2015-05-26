51841d3b32c307a24f56b8f8782ab53c9fedbbd0
missfxhr <xiaofang2016@u.northwestern.edu>
2015-05-26 15:34:50 -0500

    new test done new gitignore

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+import csv
 execfile("convert.py")
-result1, result2 = convertFile("12-13.csv")
+result = []
+for i in range(11,15):
+    result1, result2 = convertFile(str(i)+'-'+str(i+1)+'.csv')
+    result += result1
+with open("result.csv", "wb") as outputFile:
+    writer = csv.writer(outputFile)
+    writer.writerows(result)
 #return a
